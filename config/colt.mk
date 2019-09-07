8561e1aa9a2ea6ed8c9fcc1ae618be181389ad9f
nitin1438 <nitin.chobhe@gmail.com>
2019-09-07 16:38:40 +0530

    Revert "vendor: notch-city: Add 3 mode display cutout handler [2/3]"
    This reverts commit 481dddbb29f764d8f8b0fbcbd97cf99abd18b975.

--- config/colt.mk
+++ config/colt.mk
@@ -50,11 +50,6 @@ endif
   PRODUCT_PACKAGES += \
      ColtPapers
 
-# Cutout control overlays
-PRODUCT_PACKAGES += \
-    HideCutout \
-    StatusBarStock
-
 #Build Simple Gallery
   PRODUCT_PACKAGES += \
      Recorder \
